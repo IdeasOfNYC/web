137f062a28f44a30bfc49173f298afd3dc2e7aae
e-ycyang <e.yang4518@gmail.com>
2025-07-18 02:18:56 -0400

    Style: reduced sidebar idea card padding

--- app/components/SidebarIdea.tsx
+++ app/components/SidebarIdea.tsx
@@ -15,7 +15,7 @@ export const SidebarIdea: FC<SidebarIdeaProps> = ({ idea, handleOpen }) => {
       className="w-full pt-3 px-3 pb-2.5 border border-neutral-200 bg-white flex flex-col gap-3 relative group cursor-pointer hover:bg-neutral-50 rounded-lg"
       onClick={() => handleOpen(idea)}
     >
-      <h1 className="text-lg font-bold leading-tight mb-3">{idea.solution}</h1>
+      <h1 className="text-lg font-bold leading-tight">{idea.solution}</h1>
 
       <div className="flex gap-4 w-full">
         <span className="border border-neutral-200 p-1 rounded-full text-sm">{toTitleCase(idea.borough)}</span>
@@ -27,7 +27,7 @@ export const SidebarIdea: FC<SidebarIdeaProps> = ({ idea, handleOpen }) => {
             : "Advanced to BA"}
         </span>
       </div>
-      <div className="flex gap-4 w-full mb-3">
+      <div className="flex gap-5 w-full">
         <div className="flex flex-col gap-2 w-full">
           <label className="text-sm text-neutral-600 text-nowrap">
             Impact Area:
