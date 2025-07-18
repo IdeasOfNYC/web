6d8b6325145efa0cddf504c74863c578366921ab
yottayocta <chubbean.rq@gmail.com>
2025-07-17 23:03:30 -0400

    style: colors

--- app/components/SplitViewer.tsx
+++ app/components/SplitViewer.tsx
@@ -24,48 +24,57 @@ export function SplitViewer({ ideas, showLabels = false }: SplitViewerProps) {
     <div className="rounded-md border-dashed border border-neutral-300 bg-white p-4 flex flex-col items-start justify-center space-y-4 w-min">
       <div className="flex items-center space-x-2">
         <div
-          className="rounded-full bg-green-500 flex items-center justify-center text-white text-sm font-bold"
+          className="rounded-full bg-amber-300 flex items-center justify-center text-neutral-900 font-medium text-sm"
           style={{
             width: `${ideas.length / 30 + 32}px`,
+            minWidth: `${ideas.length / 30 + 32}px`,
             height: `${ideas.length / 30 + 32}px`,
+            minHeight: `${ideas.length / 30 + 32}px`,
           }}
         >
           {ideas.length}
         </div>
         {showLabels ? (
-          <span className="text-sm text-neutral-700">Submitted</span>
+          <span className="text-sm text-neutral-700 min-w-36">Submitted</span>
         ) : (
           <></>
         )}
       </div>
       <div className="flex items-center space-x-2">
         <div
-          className="w-10 h-10 rounded-full bg-yellow-500 flex items-center justify-center text-white text-sm font-bold"
+          className="w-10 h-10 rounded-full bg-green-300 flex items-center justify-center text-neutral-900 font-medium text-sm"
           style={{
             width: `${baIdeas.length / 30 + 32}px`,
+            minWidth: `${baIdeas.length / 30 + 32}px`,
             height: `${baIdeas.length / 30 + 32}px`,
+            minHeight: `${baIdeas.length / 30 + 32}px`,
           }}
         >
           {baIdeas.length}
         </div>
         {showLabels ? (
-          <span className="text-sm text-neutral-700">BA</span>
+          <span className="text-sm text-neutral-700 text-nowrap">
+            <p>Advanced to</p>
+            <p>Borough Assembly (BA)</p>
+          </span>
         ) : (
           <></>
         )}
       </div>
       <div className="flex items-center space-x-2">
         <div
-          className="w-10 h-10 rounded-full bg-red-500 flex items-center justify-center text-white text-sm font-bold"
+          className="w-10 h-10 rounded-full bg-cyan-300 flex items-center justify-center text-neutral-900 font-medium text-sm"
           style={{
             width: `${ballotIdeas.length / 30 + 32}px`,
+            minWidth: `${ballotIdeas.length / 30 + 32}px`,
             height: `${ballotIdeas.length / 30 + 32}px`,
+            minHeight: `${ballotIdeas.length / 30 + 32}px`,
           }}
         >
           {ballotIdeas.length}
         </div>
         {showLabels ? (
-          <span className="text-sm text-neutral-700">Ballot</span>
+          <span className="text-sm text-neutral-700">Added to Ballot</span>
         ) : (
           <></>
         )}
